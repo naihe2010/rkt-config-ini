7fe24a3c37d6903ed02863ddacef6fa590ec6333
Alf <naihe2010@126.com>
2015-02-25 13:55:12 +0800

    update core struct to hash of hash.

--- config-ini/ini.rkt
+++ config-ini/ini.rkt
@@ -5,12 +5,12 @@
 (define (ini-new)
   (make-hash))
 
-(define (append-ini ini entry x)
+(define (append-ini inientry x)
   (let ([matchs (string-split x "=" #:trim? #f)])
     (if (= (length matchs) 2)
-        (let ([token (string-append entry ":" (string-trim (list-ref matchs 0)))]
+        (let ([token (string-trim (list-ref matchs 0))]
               [value (string-trim (list-ref matchs 1))])
-          (hash-set! ini token value))
+          (hash-set! inientry token value))
         (error (string-append "line error:" x)))))
 
 (define (ini-read-line input)
@@ -21,28 +21,25 @@
           (ini-read-line input)
           (string-trim line)))))
 
-(define (input-ini input ini entry)
+(define (input-ini input ini inientry)
   (define x (ini-read-line input))
   (when (string? x)
     (let* ([match (regexp-match #px"^\\s*\\[(.+)\\]\\s*$" x)])
       (if (not match)
-          (append-ini ini entry x)
-          (set! entry (list-ref match 1))))
-    (input-ini input ini entry)))
+          (append-ini inientry x)
+          (begin
+            (set! inientry (make-hash))
+            (hash-set! ini (list-ref match 1) inientry)))
+      (input-ini input ini inientry))))
 
 (define (output-ini output ini)
-  (letrec ([entry ""])
-    (for ([i (sort (hash-keys ini) string<?)])
-      (let* ([nlist (string-split i ":")]
-             [tentry (list-ref nlist 0)]
-             [ttoken (list-ref nlist 1)]
-             [value (hash-ref ini i)])
-        (if (string=? entry tentry)
-            (displayln (string-append ttoken " = " value) output)
-            (begin
-              (displayln (string-append "[" tentry "]") output)
-              (displayln (string-append ttoken " = " value) output)
-              (set! entry tentry)))))))
+  (hash-for-each ini
+                 (lambda (entry entryini)
+                   (displayln (string-append "[" entry "]") output)
+                   (hash-for-each entryini
+                                  (lambda (token value)
+                                    (displayln (string-append token " = " value) output)))
+                   (displayln "" output))))
 
 (define (ini-read ini path)
   (call-with-input-file path
@@ -56,24 +53,18 @@
     #:exists 'truncate))
 
 (define (ini-sections ini)
-  (let ([tokens (make-hash)])
-    (hash-for-each ini
-                   (lambda (k v)
-                     (hash-set! tokens
-                                (list-ref (string-split k ":") 0)
-                                1)))
-    (hash-keys tokens)))
+  (hash-keys ini))
 
 (define (ini-has-key? ini entry key)
-  (hash-has-key? ini (string-append entry ":" key)))
+  (if (hash-has-key? ini entry)
+      (hash-has-key? (hash-ref ini entry) key)
+      #f))
 
 (define (ini-has-section? ini entry)
-  (if
-   (findf (lambda (e) (string=? e entry)) (ini-sections ini))
-   #t #f))
+  (hash-has-key? ini entry))
 
 (define (ini-get-key-string ini entry key)
-  (hash-ref ini(string-append entry ":" key)))
+  (hash-ref (hash-ref ini entry) key))
 
 (define (ini-get-key-boolean ini entry key)
   (let* ([str (ini-get-key-string ini entry key)]
@@ -85,17 +76,21 @@
     (string->number str)))
 
 (define (ini-set-key ini entry key value)
-  (hash-set! ini (string-append entry ":" key) value))
+  (cond
+    [(string? value)
+     (hash-set! (hash-ref ini entry) key value)]
+    [(number? value)
+     (hash-set! (hash-ref ini entry) key (number->string value))]
+    [(boolean? value)
+     (hash-set! (hash-ref ini entry) key (if value "1" "0"))]
+    [else
+     (error (string-append "Not supported data type of:" value))]))
 
 (define (ini-remove-key ini entry key)
-  (hash-remove! ini (string-append entry ":" key)))
+  (hash-remove! (hash-ref ini entry) key))
 
 (define (ini-remove-section ini entry)
-  (hash-for-each ini
-                 (lambda (k v)
-                   (let* ([matchs (string-split k ":")])
-                     (when (string=? (list-ref matchs 0) entry)
-                       (hash-remove! ini k))))))
+  (hash-remove! ini entry))
 
 ;; Provides
 (provide
